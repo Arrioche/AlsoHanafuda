803e4a6dfda46fabd75a721ee5a68b6f9166d74c
Arrioche <kittydurmont@gmail.com>
2018-10-16 09:57:05 -0700

    I have commented and organized everything, and also cleaned up the method of dropping cards on the board

---
+++ app/src/main/java/com/tnielsen9082/alsohanafuda/Splitter.java
@@ -0,0 +1,11 @@
+package com.tnielsen9082.alsohanafuda;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splitter extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
