f7e44ccdb736a2cfdd2f15c6318be2f42e99dc83
Arrioche <kittydurmont@gmail.com>
2019-02-08 09:55:02 -0800

    I messed with the graphics a bit

---
+++ app/src/main/java/com/tnielsen9082/alsohanafuda/ComboButton.java
@@ -0,0 +1,10 @@
+package com.tnielsen9082.alsohanafuda;
+
+import android.view.View;
+
+public class ComboButton implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
